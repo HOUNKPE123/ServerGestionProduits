78775a60ff81cd30751c9666b336b873e291c185
hkpgameDev <98496506+Razmakul@users.noreply.github.com>
2023-02-06 19:58:15 +0000

    frist commit

---
+++ src/test/java/com/example/ServerGestionProduits/ServerGestionProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServerGestionProduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerGestionProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
